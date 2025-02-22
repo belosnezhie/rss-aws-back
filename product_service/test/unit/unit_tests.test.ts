475a883dd4e79979eb12a5e7d39013ce19e6d8de
Maria Treier <mariatraier@gmail.com>
2025-02-22 15:42:28 +0100

    feat: implement getProductsList test

--- product_service/test/unit/unit_tests.test.ts
+++ product_service/test/unit/unit_tests.test.ts
@@ -1,5 +1,23 @@
 import { test } from '@jest/globals';
+import { handler } from '../../lambda_functions/getProductsList';
+import { APIGatewayProxyEvent, Context, APIGatewayProxyResult } from 'aws-lambda';
+import products from '../../lambda_functions/products_mock';
 
-test('test', () => {
-  expect(1).toBe(1);
+test('test', async () => {
+  const expected = {
+    statusCode: 200,
+    headers: {
+      'Access-Control-Allow-Origin': '*',
+      'Access-Control-Allow-Methods': 'GET',
+    },
+    body: JSON.stringify(products),
+  }
+
+  const event = {} as APIGatewayProxyEvent;
+  const context = {} as Context;
+
+  const actual = await handler(event, context, () => { }) as APIGatewayProxyResult;
+  expect(actual.statusCode).toBe(expected.statusCode);
+  // expect(actual.headers).toBe(expected.headers);
+  expect(actual.body).toBe(expected.body);
 })
