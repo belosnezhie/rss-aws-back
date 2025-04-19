2fbc7c72a46303de04d46c18821fc7b78105258c
Maria Treier <mariatraier@gmail.com>
2025-04-19 23:37:59 +0200

    feat: create product proxy

--- bff-service/src/app.controller.ts
+++ bff-service/src/app.controller.ts
@@ -1,5 +1,6 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Req, Query, Body, Headers } from '@nestjs/common';
 import { AppService } from './app.service';
+import axios, { AxiosHeaders } from 'axios';
 
 @Controller()
 export class AppController {
@@ -8,5 +9,29 @@ export class AppController {
   @Get()
   getHello(): string {
     return this.appService.getHello();
+  }
+
+  @Get('/product')
+  async getProduct(@Headers() headers): Promise<any> {
+    console.log(headers);
+
+    const axiosHeaders = new AxiosHeaders(headers);
+    axiosHeaders.delete('host');
+
+    const res = await axios.get(
+      `${process.env.PRODUCT}/products`,
+      {
+        headers: axiosHeaders,
+      }
+    );
+    console.log(res.data)
+    return res.data;
+  }
+
+  @Get('/cart')
+  async getCart(): Promise<any> {
+    const response = await fetch('XXXXXXXXXXXXXXXXXXXXXXXXXXXXX');
+    const data = await response.json();
+    return data;
   }
 }
